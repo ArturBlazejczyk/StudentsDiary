b0a872766594bd7473a65354162d0a0c5598cdc8
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-14 19:26:30 +0100

    serialization and deserialization methods

--- StudentsDiary/Main.cs
+++ StudentsDiary/Main.cs
@@ -8,15 +8,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace StudentsDiary
 {
     public partial class Main : Form
     {
+        private string _filePath = 
+            Path.Combine(Environment.CurrentDirectory, "students.txt");
+
         public Main()
         {
             InitializeComponent();
 
+        }
+
+        public void SerializeToFile(List<Student> students)
+        {
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streamWriter = new StreamWriter(_filePath))
+            {
+                serializer.Serialize(streamWriter, students);
+                streamWriter.Close();
+            }
+        }
+
+        public List<Student> DeserializeFromFile() 
+        {
+            if (!File.Exists(_filePath))
+                return new List<Student>();
+
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var students = (List<Student>)serializer.Deserialize(streamReader);
+                streamReader.Close();
+                return students;
+            }    
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
